058f844b099083314c8c68b8669d4b973bfc6a06
Lady Cans <frc2881@gmail.com>
2025-12-04 18:49:56 -0600

    Fix doc comments to not complain on deploy

--- scripts/calculateRobotToCameraTransform.py
+++ scripts/calculateRobotToCameraTransform.py
@@ -1,47 +1,44 @@
 from wpimath.geometry import Pose3d, Transform3d, Translation3d, Rotation3d, Quaternion
 
-"""
-Use this script for empirical calculation of robot to camera transform 
-for each pose sensor configuration using fixed target measured 
-from robot and targetPose averages from PhotonVision.
+# Use this script for empirical calculation of robot to camera transform 
+# for each pose sensor configuration using fixed target measured 
+# from robot and targetPose averages from PhotonVision.
 
-Steps:
-1) Place the robot in a fixed position with the target camera facing 
-a single AprilTag target with the center of robot precisely aligned 
-with the center of the AprilTag (so that the Y translation below is zero)
+# Steps:
+# 1) Place the robot in a fixed position with the target camera facing 
+# a single AprilTag target with the center of robot precisely aligned 
+# with the center of the AprilTag (so that the Y translation below is zero)
 
-2) Accurately measure the distance from the center of the robot to
-the face of the AprilTag to set the X translation value below
+# 2) Accurately measure the distance from the center of the robot to
+# the face of the AprilTag to set the X translation value below
 
-3) Accurately measure the distance from the floor (base of the roobt)
-to the center of the AprilTag to set the Z translation value below
+# 3) Accurately measure the distance from the floor (base of the roobt)
+# to the center of the AprilTag to set the Z translation value below
 
-4) Using PhotonVision web UI, confirm that the target AprilTag is being
-detected and the camera to target translation values are being reported
+# 4) Using PhotonVision web UI, confirm that the target AprilTag is being
+# detected and the camera to target translation values are being reported
 
-5) Using AdvantageScope, connect to the robot and navigate to the "targetPose"
-topic category for the camera transform being calculated. Create a new
-"Statistics" tab with the "Measurements" section clear for new entries.
-Under both translation and rotation sections under targetPose, drag each
-individual value into the Measurements section so that the calculation of
-averages can begin. Wait at least 30 seconds for the Median value for each
-translation and rotation property to settle. Use each of those Median values
-to set the appropriate constant value below for target to camera transform.
+# 5) Using AdvantageScope, connect to the robot and navigate to the "targetPose"
+# topic category for the camera transform being calculated. Create a new
+# "Statistics" tab with the "Measurements" section clear for new entries.
+# Under both translation and rotation sections under targetPose, drag each
+# individual value into the Measurements section so that the calculation of
+# averages can begin. Wait at least 30 seconds for the Median value for each
+# translation and rotation property to settle. Use each of those Median values
+# to set the appropriate constant value below for target to camera transform.
 
-6) After all of the script constant values have been set, save the script
-and open a new Terminal window in VSCode. After the Python virtual environment 
-activates, run the script with the following command (without quotes):
-"python .\lib\scripts\calculateRobotToCameraTransform.py"
+# 6) After all of the script constant values have been set, save the script
+# and open a new Terminal window in VSCode. After the Python virtual environment 
+# activates, run the script with the following command (without quotes):
+# "python .\lib\scripts\calculateRobotToCameraTransform.py"
 
-7) Copy the Transform3d value that is printed to the terminal console and 
-paste it into the appropriate place in the robot constants for the associated 
-pose sensor configuration being calculated here.
+# 7) Copy the Transform3d value that is printed to the terminal console and 
+# paste it into the appropriate place in the robot constants for the associated 
+# pose sensor configuration being calculated here.
 
-Example: Transform3d(Translation3d(0.149506, -0.055318, 0.271137), Rotation3d(-0.001852, -0.181301, 0.020370))
+# Example: Transform3d(Translation3d(0.149506, -0.055318, 0.271137), Rotation3d(-0.001852, -0.181301, 0.020370))
 
-8) Repeat this same process from step 1 above for all other pose sensor cameras on the robot
-
-"""
+# 8) Repeat this same process from step 1 above for all other pose sensor cameras on the robot
 
 # Note: the constant values below are for example only - see the steps above for setting real/correct values
 
