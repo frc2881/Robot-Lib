ddb6fdbfed2af677d8e4b47ff20da9ffdfc858d2
Lady Cans <frc2881@gmail.com>
2025-12-12 13:33:21 -0600

    Update script documentation after working through process on offseason robot.

--- scripts/calculateRobotToCameraTransform.py
+++ scripts/calculateRobotToCameraTransform.py
@@ -7,45 +7,52 @@ from wpimath.geometry import Pose3d, Transform3d, Translation3d, Rotation3d, Qua
 # Steps:
 # 1) Place the robot in a fixed position with the target camera facing 
 # a single AprilTag target with the center of robot precisely aligned 
-# with the center of the AprilTag (so that the Y translation below is zero)
+# with the center of the AprilTag (so that the Y translation below is zero).
+# Best approach is to create a rectangle on the floor with gaffer tape 
+# with the robot frame parallel on one side and the AprilTag (mounted) 
+# on the other side to ensure the two are parallel.
 
 # 2) Accurately measure the distance from the center of the robot to
-# the face of the AprilTag to set the X translation value below
+# the face of the AprilTag to set the X translation value below.
 
 # 3) Accurately measure the distance from the floor (base of the roobt)
-# to the center of the AprilTag to set the Z translation value below
+# to the center of the AprilTag to set the Z translation value below.
 
 # 4) Using PhotonVision web UI, confirm that the target AprilTag is being
-# detected and the camera to target translation values are being reported
+# detected and the target translation values are being reported correctly.
 
-# 5) Using AdvantageScope, connect to the robot and navigate to the "targetPose"
-# topic category for the camera transform being calculated. Create a new
-# "Statistics" tab with the "Measurements" section clear for new entries.
-# Under both translation and rotation sections under targetPose, drag each
-# individual value into the Measurements section so that the calculation of
-# averages can begin. Wait at least 30 seconds for the Median value for each
-# translation and rotation property to settle. Use each of those Median values
-# to set the appropriate constant value below for target to camera transform.
+# 5) Using AdvantageScope, connect to the robot and expand the photonvision
+# topic, then expand the camera name being calculated, then expand to the 
+# "targetPose", and finally expand both translation and rotation/quaternion. 
+# Create a new "Statistics" panel in AdvantageScope with an empty "Measurements" 
+# section. Under both translation and rotation sections under targetPose, drag each 
+# individual value into the Measurements section so that the calculation of 
+# median averages can begin. Wait at least 30 seconds for the Median value for each
+# translation (x, y, z) and rotation (w, x, y, z) property to settle. Use each of 
+# those Median values to set the appropriate constant value in the script 
+# below for target to camera transform calculation.
 
 # 6) After all of the script constant values have been set, save the script
-# and open a new Terminal window in VSCode. After the Python virtual environment 
-# activates, run the script with the following command (without quotes):
-# "python .\lib\scripts\calculateRobotToCameraTransform.py"
+# and open a new Terminal window in VSCode (do not commit the script changes in git). 
+# After the Python virtual environment activates, copy, paste, and execute the 
+# script with the following command: 
+# python .\lib\scripts\calculateRobotToCameraTransform.py
 
 # 7) Copy the Transform3d value that is printed to the terminal console and 
 # paste it into the appropriate place in the robot constants for the associated 
 # pose sensor configuration being calculated here.
 
-# Example: Transform3d(Translation3d(0.149506, -0.055318, 0.271137), Rotation3d(-0.001852, -0.181301, 0.020370))
+# Example output from example values below: 
+# Transform3d(Translation3d(0.149506, -0.055318, 0.271137), Rotation3d(-0.001852, -0.181301, 0.020370))
 
-# 8) Repeat this same process from step 1 above for all other pose sensor cameras on the robot
+# 8) Repeat this same process from step 1 above for all other pose sensor cameras on the robot.
 
-# Note: the constant values below are for example only - see the steps above for setting real/correct values
+# Note: the constant values below are for example only - see the steps above for setting real/correct values.
 
 TARGET_TO_ROBOT_TRANSLATION_X = 0.860 # distance from center of robot to face of AprilTag
-TARGET_TO_ROBOT_TRANSLATION_Y = 0.0 # distance from center of robot to center of AprilTag (should be zero)
-TARGET_TO_ROBOT_TRANSLATION_Z = -0.311 # distance from floor to center of AprilTag
-TARGET_TO_ROBOT_ROTATION_YAW = -180.0 # rotation of camera on robot to AprilTag (e.g. front facing camera/front of robot facing toward AprilTag)
+TARGET_TO_ROBOT_TRANSLATION_Y = 0.0 # distance from center of robot to center of AprilTag (should be zero using alignment method above)
+TARGET_TO_ROBOT_TRANSLATION_Z = -0.311 # distance from center of AprilTag to the floor (will be negative value as the robot is below the AprilTag)
+TARGET_TO_ROBOT_ROTATION_YAW = 180.0 # rotation of camera on robot to AprilTag (e.g. front facing camera/front of robot turned 180 degrees to face toward AprilTag, rear of robot will be 0 degrees, sides will be -90/90)
 
 TARGET_TO_CAMERA_TRANSLATION_X = 0.707 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
 TARGET_TO_CAMERA_TRANSLATION_Y = 0.041 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
