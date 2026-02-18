0a6a4fe014e921967240af9f8212b56460a69b12
Lady Cans <frc2881@gmail.com>
2026-02-18 12:40:32 -0600

    Update camera transform calibration script to export human-readable constant for robot import

--- scripts/calculateRobotToCameraTransform.py
+++ scripts/calculateRobotToCameraTransform.py
@@ -6,15 +6,11 @@ from wpimath.geometry import Pose3d, Transform3d, Translation3d, Rotation3d, Qua
 # from robot and targetPose averages from PhotonVision.
 
 # Steps:
-# 1) Place the robot in a fixed position with the target camera facing 
-# a single AprilTag target with the center of robot precisely aligned 
-# with the center of the AprilTag (so that the Y translation below is zero).
-# Best approach is to create a rectangle on the floor with gaffer tape 
-# with the robot frame parallel on one side and the AprilTag (mounted) 
-# on the other side to ensure the two are parallel.
+# 1) Place the robot in a fixed measured position with the target camera facing 
+# a single AprilTag target.
 
-# 2) Accurately measure the distance from the center of the robot to
-# the face of the AprilTag to set the X translation value below.
+# 2) Accurately measure the distances from the center of the robot to
+# the face of the AprilTag to set the X and Y translation values below.
 
 # 3) Accurately measure the distance from the floor (base of the roobt)
 # to the center of the AprilTag to set the Z translation value below.
@@ -43,25 +39,36 @@ from wpimath.geometry import Pose3d, Transform3d, Translation3d, Rotation3d, Qua
 # paste it into the appropriate place in the robot constants for the associated 
 # pose sensor configuration being calculated here.
 
-# Example output from example values below: 
-# Transform3d(Translation3d(0.149506, -0.055318, 0.271137), Rotation3d(-0.001852, -0.181301, 0.020370))
+# Example output: 
+# Transform3d(
+#   Translation3d(x = units.inchesToMeters(12.35), y = units.inchesToMeters(-12.75), z = units.inchesToMeters(11.67)), 
+#   Rotation3d(roll = units.degreesToRadians(1.39), pitch = units.degreesToRadians(-15.56), yaw = units.degreesToRadians(-32.15))
+# )
 
 # 8) Repeat this same process from step 1 above for all other pose sensor cameras on the robot.
 
 # Note: the constant values below are for example only - see the steps above for setting real/correct values.
 
-TARGET_TO_ROBOT_TRANSLATION_X: units.meters = 1.5748 # distance from center of robot to face of AprilTag
-TARGET_TO_ROBOT_TRANSLATION_Y: units.meters = -0.381 # distance from center of robot to center of AprilTag (should be zero using alignment method above)
-TARGET_TO_ROBOT_TRANSLATION_Z: units.meters = -1.12395 # distance from center of AprilTag to the floor (will be negative value as the robot is below the AprilTag)
-TARGET_TO_ROBOT_ROTATION_YAW: units.degrees = 180.0 # rotation of robot to AprilTag (e.g. front of robot turned to face toward AprilTag is 180 degrees, rear of robot facing tag will be 0 degrees, sides will be -90/90 accordingly)
+# distance from center of AprilTag to center of robot
+TARGET_TO_ROBOT_TRANSLATION_X: units.meters = 1.5748
 
-TARGET_TO_CAMERA_TRANSLATION_X = 1.154 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_TRANSLATION_Y = 0.894 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_TRANSLATION_Z = 0.384 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_QUATERNION_W = 0.386 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_QUATERNION_X = -0.187 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_QUATERNION_Y = 0.062 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
-TARGET_TO_CAMERA_QUATERNION_Z = -0.901 # calculated/average median value under targetPose reported by PhotonVision via AdvantageScope statistics
+# distance from center of AprilTag to center of robot
+TARGET_TO_ROBOT_TRANSLATION_Y: units.meters = -0.381
+
+# distance from center of AprilTag to the floor (will be negative value as the robot is below the AprilTag)
+TARGET_TO_ROBOT_TRANSLATION_Z: units.meters = -1.12395 
+
+# rotation of the AprilTag to the robot (e.g. front of robot turned to face toward AprilTag is 180 degrees, rear of robot facing tag will be 0 degrees, sides will be -90/90 accordingly)
+TARGET_TO_ROBOT_ROTATION_YAW: units.degrees = 180.0 
+
+# calculated median average values under targetPose for the specific camera as reported by PhotonVision via AdvantageScope statistics
+TARGET_TO_CAMERA_TRANSLATION_X = 1.154 
+TARGET_TO_CAMERA_TRANSLATION_Y = 0.894
+TARGET_TO_CAMERA_TRANSLATION_Z = 0.384
+TARGET_TO_CAMERA_QUATERNION_W = 0.386
+TARGET_TO_CAMERA_QUATERNION_X = -0.187
+TARGET_TO_CAMERA_QUATERNION_Y = 0.062
+TARGET_TO_CAMERA_QUATERNION_Z = -0.901
 
 # ===========================================================================
 
@@ -72,10 +79,12 @@ targetToRobot = Transform3d(
 
 targetToCamera = Transform3d(
   Translation3d(TARGET_TO_CAMERA_TRANSLATION_X, TARGET_TO_CAMERA_TRANSLATION_Y, TARGET_TO_CAMERA_TRANSLATION_Z), 
-  Rotation3d(Quaternion(TARGET_TO_CAMERA_QUATERNION_W, TARGET_TO_CAMERA_QUATERNION_X, TARGET_TO_CAMERA_QUATERNION_Y, TARGET_TO_CAMERA_QUATERNION_Z)))
+  Rotation3d(Quaternion(TARGET_TO_CAMERA_QUATERNION_W, TARGET_TO_CAMERA_QUATERNION_X, TARGET_TO_CAMERA_QUATERNION_Y, TARGET_TO_CAMERA_QUATERNION_Z))
+)
 
 robotToCamera = Pose3d().transformBy(targetToCamera.inverse()) - Pose3d().transformBy(targetToRobot)
 
-constant = str(robotToCamera).replace("x=", "").replace("y=", "").replace("z=", "")
-
-print(constant)
+print(f'''Transform3d(
+  Translation3d(x = units.inchesToMeters({"%.2f" % round(units.metersToInches(robotToCamera.translation().X()), 2)}), y = units.inchesToMeters({"%.2f" % round(units.metersToInches(robotToCamera.translation().Y()), 2)}), z = units.inchesToMeters({"%.2f" % round(units.metersToInches(robotToCamera.translation().Z()), 2)})), 
+  Rotation3d(roll = units.degreesToRadians({"%.2f" % round(units.radiansToDegrees(robotToCamera.rotation().X()), 2)}), pitch = units.degreesToRadians({"%.2f" % round(units.radiansToDegrees(robotToCamera.rotation().Y()), 2)}), yaw = units.degreesToRadians({"%.2f" % round(units.radiansToDegrees(robotToCamera.rotation().Z()), 2)}))
+)''')
