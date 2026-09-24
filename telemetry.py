704eb43c789f0c87cddd5c1e9b7f3da19e1982c1
Lady Cans <frc2881@gmail.com>
2026-09-24 15:16:05 -0500

    Fix list length check for telemetry logging

--- telemetry.py
+++ telemetry.py
@@ -35,12 +35,13 @@ def log(name: str, value: Any) -> None:
     case int() | float():
       SmartDashboard.putNumber(name, value)
     case list():
-      match value[0]:
-        case str():
-          SmartDashboard.putStringArray(name, value)
-        case bool():
-          SmartDashboard.putBooleanArray(name, value)
-        case int() | float():
-          SmartDashboard.putNumberArray(name, value)
+      if value:
+        match value[0]:
+          case str():
+            SmartDashboard.putStringArray(name, value)
+          case bool():
+            SmartDashboard.putBooleanArray(name, value)
+          case int() | float():
+            SmartDashboard.putNumberArray(name, value)
     case _:
       pass
